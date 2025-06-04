535bdc0191f14d5983a63081a8d6ba7292c46125
Dicky Wahyu Ramadani <ramadani.wahyu@gmail.com>
2025-06-04 08:45:12 +0700

    fix api

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,12 +7,13 @@ const protectRoute = async (req, res, next) => {
         //get token
         const token = req.header("Authorization").replace("Bearer ", "");
         if (!token) return res.status(401).json({message: "Access denied!"});
-
+        
         //verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
+        
         // find user
-        const user = await User.findById(decode.userId).select("-password");
+        const user = await User.findById(decoded.userId).select("-password");
+        
         if (!user) return res.status(401).json({message: "Invalid authentication."});
 
         req.user = user;
@@ -22,5 +23,5 @@ const protectRoute = async (req, res, next) => {
         res.status(500).json({message: error.message});
     }
 };
-
+// 6839b5a0eb8f4a6ec679c6cb
 export default protectRoute;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -80,7 +80,8 @@ router.post("/login", async  (req, res) => {
                 id : user._id,
                 username: user.username,
                 email: user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
             },
         });
 
